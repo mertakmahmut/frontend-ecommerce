a75a9721a64d8219d5fc4262dd9466c6aeb12f5a
mertakmahmut <mertakmahmut@icloud.com>
2025-04-19 18:25:17 +0300

    header kısmı bitti

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { MdEmail } from "react-icons/md";
 import { IoMdPhonePortrait } from "react-icons/io";
-import { FaFacebookF, FaLock, FaUser, FaList } from "react-icons/fa";
+import { FaFacebookF, FaList, FaLock, FaUser } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
@@ -13,8 +13,8 @@ import { FaPhoneAlt } from "react-icons/fa";
 import { IoIosArrowDown } from "react-icons/io"; 
 
 const Header = () => {
-
     const {pathname} = useLocation()
+     
     const [showShidebar, setShowShidebar] = useState(true);
     const [categoryShow, setCategoryShow] = useState(true);
     const user = true
@@ -46,68 +46,66 @@ const Header = () => {
 
                             <li className='flex relative justify-center items-center gap-2 text-sm '>
                                 <span><IoMdPhonePortrait  /></span>
-                                <span>+(90) 555 555 5555 </span>
-                            </li>
-
+                                <span>+(90)5554443333</span>
+                            </li> 
                         </ul>
 
                         <div>
                             <div className='flex justify-center items-center gap-10'>
-                                 <div className='flex justify-center items-center gap-4 text-black'>
-                                     <a href="#"><FaFacebookF /></a>
-                                     <a href="#"><FaTwitter /> </a>
-                                     <a href="#"><FaLinkedin /></a>
-                                     <a href="#"><FaGithub /> </a> 
-                                 </div>
-                                    <div className='flex group cursor-pointer text-slate-800 text-sm justify-center items-center gap-1 relative after:h-[18px] after:w-[1px] after:bg-[#afafaf] after:-right-[16px] after:absolute before:absolute before:h-[18px] before:bg-[#afafaf] before:w-[1px] before:-left-[20px]'>
-                                        <img src="http://localhost:3000/images/language.png" alt="" />
-                                        <span><IoMdArrowDropdown /></span>
-                                        <ul className='absolute invisible transition-all top-12 rounded-sm duration-200 text-white p-2 w-[100px] flex flex-col gap-3 group-hover:visible group-hover:top-6 group-hover:bg-black z-10'>
-                                            <li>Türkçe</li>
-                                            <li>English</li>
-                                        </ul>
-                                    </div>
+                                <div className='flex justify-center items-center gap-4 text-black'>
+                                    <a href="#"><FaFacebookF /></a>
+                                    <a href="#"><FaTwitter /> </a>
+                                    <a href="#"><FaLinkedin /></a>
+                                    <a href="#"><FaGithub /> </a> 
+                                </div>
+                                <div className='flex group cursor-pointer text-slate-800 text-sm justify-center items-center gap-1 relative after:h-[18px] after:w-[1px] after:bg-[#afafaf] after:-right-[16px] after:absolute before:absolute before:h-[18px] before:bg-[#afafaf] before:w-[1px] before:-left-[20px]'>
+                                    <img src="http://localhost:3000/images/language.png" alt="" />
+                                    <span><IoMdArrowDropdown /></span>
+                                    <ul className='absolute invisible transition-all top-12 rounded-sm duration-200 text-white p-2 w-[100px] flex flex-col gap-3 group-hover:visible group-hover:top-6 group-hover:bg-black z-10'>
+                                    <li>Türkçe</li>
+                                    <li>English</li>
+                                    </ul>
+                                </div>
 
-                                    {
-                                        user ? <Link className='flex cursor-pointer justify-center items-center gap-2 text-sm text-black' to='/dashboard'>
+                                {
+                                    user ? <Link className='flex cursor-pointer justify-center items-center gap-2 text-sm text-black' to='/dashboard'>
                                         <span> <FaUser/> </span>
-                                        <span>Mert Akmahmut</span>
+                                        <span>Mert Akmahmut </span>
                                         </Link> : <Link className='flex cursor-pointer justify-center items-center gap-2 text-sm text-black' to='/login'>
                                         <span> <FaLock /> </span>
                                         <span>Login </span>
                                         </Link>
-                                    }
+                                }
  
                             </div>
-                        </div>
-
-                    </div>
-
-                </div>
-
+                        </div> 
+                    </div> 
+                </div> 
             </div>
+
 
             <div className='w-white'>
                 <div className='w-[85%] lg:w-[90%] mx-auto'>
                     <div className='h-[80px] md-lg:h-[100px] flex justify-between items-center flex-wrap'>
+                    
                         <div className='md-lg:w-full w-3/12 md-lg:pt-4'>
                             <div className='flex justify-between items-center'>
                                 <Link to='/'>
                                     <img className='h-[50px]' src="http://localhost:3000/images/adminLogo.png" alt="" />
                                 </Link>
                                 <div className='justify-center items-center w-[30px] h-[30px] bg-white text-slate-600 border border-slate-600 rounded-sm cursor-pointer lg:hidden md-lg:flex xl:hidden hidden' onClick={() => setShowShidebar(false)}>
-                                    <span><FaList/> </span>
+                                    <span> <FaList/> </span>
                                 </div>
-                            </div>
+                            </div> 
                         </div>
-
+        
                         <div className='md:lg:w-full w-9/12'>
                             <div className='flex justify-between md-lg:justify-center items-center flex-wrap pl-8'>
                                 <ul className='flex justify-start items-start gap-8 text-sm font-bold uppercase md-lg:hidden'>
                                     <li>
                                         <Link className={`p-2 block ${pathname === '/' ?  'text-[#059473]' : 'text-slate-600' } `} >Home</Link>
                                     </li>
-            
+
                                     <li>
                                         <Link className={`p-2 block ${pathname === '/shop' ?  'text-[#059473]' : 'text-slate-600' } `} >Shop</Link>
                                     </li>
@@ -120,7 +118,7 @@ const Header = () => {
                                     <li>
                                         <Link className={`p-2 block ${pathname === '/contact' ?  'text-[#059473]' : 'text-slate-600' } `} >Contact Us</Link>
                                     </li>
-            
+
                                 </ul>
 
                                 <div className='flex md-lg:hidden justify-center items-center gap-5'>
@@ -131,33 +129,29 @@ const Header = () => {
                                                 {
                                                     wishlist_count
                                                 }
+
                                             </div>
                                         </div>
-    
+
                                         <div className='relative flex justify-center items-center cursor-pointer w-[35px] h-[35px] rounded-full bg-[#e2e2e2]'>
                                             <span className='text-xl text-green-500'><FaCartShopping  /></span>
                                             <div className='w-[20px] h-[20px] absolute bg-red-500 rounded-full text-white flex justify-center items-center -top-[3px] -right-[5px] '>
                                                 {
                                                     wishlist_count
                                                 }
-    
-                                            </div>
-                                        </div>
-    
-                                    </div>
- 
-                                </div>
-            
-                            </div>
-                        
+                                            </div> 
+                                        </div> 
+                                    </div> 
+                                </div> 
+                            </div> 
                         </div>
-                    </div>
-
+                    </div> 
                 </div>
             </div>
 
+
             <div className='hidden md-lg:block'>
-                <div onClick={()=> setShowShidebar(true)} className={`flxed duration-200 transition-all ${showShidebar ? 'invisible' : 'visible'} hidden md-lg:block w-screen h-screen bg-[rgba(0,0,0,0.5)] top-0 left-0 z-20 `}>  
+                <div onClick={()=> setShowShidebar(true)} className={`fixed duration-200 transition-all ${showShidebar ? 'invisible' : 'visible'} hidden md-lg:block w-screen h-screen bg-[rgba(0,0,0,0.5)] top-0 left-0 z-20 `}>  
                 </div> 
 
                 <div className={`w-[300px] z-[9999] transition-all duration-200 fixed ${showShidebar ? '-left-[300px]' : 'left-0 top-0'} overflow-y-auto bg-white h-screen py-6 px-8 `}>
@@ -167,12 +161,12 @@ const Header = () => {
                         </Link>
                         <div className='flex justify-start items-center gap-10'>
                             <div className='flex group cursor-pointer text-slate-800 text-sm justify-center items-center gap-1 relative after:h-[18px] after:w-[1px] after:bg-[#afafaf] after:-right-[16px] after:absolute '>
-                                    <img src="http://localhost:3000/images/language.png" alt="" />
-                                    <span><IoMdArrowDropdown /></span>
-                                    <ul className='absolute invisible transition-all top-12 rounded-sm duration-200 text-white p-2 w-[100px] flex flex-col gap-3 group-hover:visible group-hover:top-6 group-hover:bg-black z-10'>
-                                    <li>Türkçe</li>
-                                    <li>English</li>
-                                    </ul>
+                                <img src="http://localhost:3000/images/language.png" alt="" />
+                                <span><IoMdArrowDropdown /></span>
+                                <ul className='absolute invisible transition-all top-12 rounded-sm duration-200 text-white p-2 w-[100px] flex flex-col gap-3 group-hover:visible group-hover:top-6 group-hover:bg-black z-10'>
+                                <li>Türkçe</li>
+                                <li>English</li>
+                                </ul>
                             </div>
                             {
                                 user ? <Link className='flex cursor-pointer justify-center items-center gap-2 text-sm text-black' to='/dashboard'>
@@ -203,41 +197,35 @@ const Header = () => {
                             <li>
                                 <Link className={`py-2 block ${pathname === '/contact' ?  'text-[#059473]' : 'text-slate-600' } `} >Contact Us</Link>
                             </li>
-
                         </ul>
-
                         <div className='flex justify-start items-center gap-4 text-black'>
                             <a href="#"><FaFacebookF /></a>
                             <a href="#"><FaTwitter /> </a>
                             <a href="#"><FaLinkedin /></a>
                             <a href="#"><FaGithub /> </a> 
                         </div>
- 
+
                         <div className='w-full flex justify-end md-lg:justify-start gap-3 items-center'>
                             <div className='w-[48px] h-[48px] rounded-full flex bg-[#f5f5f5] justify-center items-center '>
                             <span><FaPhoneAlt /></span>
                             </div>
                             <div className='flex justify-end flex-col gap-1'>
-                                <h2 className='text-sm font-medium text-slate-700'>+9055544433333</h2>
+                                <h2 className='text-sm font-medium text-slate-700'>+905554443333</h2>
                                 <span className='text-xs'>Support 24/7</span> 
                             </div>
                         </div>
-                
+
                         <ul className='flex flex-col justify-start items-start gap-3 text-[#1c1c1c]'>
                             <li className='flex justify-start items-center gap-2 text-sm'>
                                 <span><MdEmail /></span>
                                 <span>support@gmail.com</span>
                             </li>
-                        </ul>
-
-
+                        </ul> 
 
                     </div> 
-                </div> 
-
-
-
+                </div>  
             </div>
+
 
             <div className='w-[85%] lg:w-[90%] mx-auto'>
                 <div className='flex w-full flex-wrap md-lg:gap-8'>
@@ -264,6 +252,7 @@ const Header = () => {
                                     }
                                 </ul>
                             </div>
+
                         </div>
                     </div>
 
@@ -281,15 +270,13 @@ const Header = () => {
                                             }
                                         </select>
                                     </div>
-                                    
                                     <input className='w-full relative bg-transparent text-slate-500 outline-0 px-3 h-full' onChange={(e)=> setSearchValue(e.target.value)} type="text" name='' id='' placeholder='What do you need' />
                                     <button className='bg-[#059473] right-0 absolute px-8 h-full font-semibold uppercase text-white'>Search</button>
-                                </div>
-
+                                </div> 
                             </div>
 
                             <div className='w-4/12 block md-lg:hidden pl-2 md-lg:w-full md-lg:pl-0'>
- 
+
                                 <div className='w-full flex justify-end md-lg:justify-start gap-3 items-center'>
                                     <div className='w-[48px] h-[48px] rounded-full flex bg-[#f5f5f5] justify-center items-center '>
                                         <span><FaPhoneAlt /></span>
@@ -301,13 +288,18 @@ const Header = () => {
                                 </div>
 
                             </div>
-                        </div>
-                    </div>
-                </div>
 
+
+                        </div>
+                    </div>    
+                </div> 
             </div>
 
-        
+
+
+
+
+           
         </div>
     );
 };
