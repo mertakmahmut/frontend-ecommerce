03973b37c8e1affd2b4cbdc68b1d8e95a2af9f14
mertakmahmut <mertakmahmut@icloud.com>
2025-04-19 15:59:03 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { MdEmail } from "react-icons/md";
+import { IoMdPhonePortrait } from "react-icons/io";
+import { FaFacebookF, FaLock, FaUser } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { IoMdArrowDropdown } from "react-icons/io";
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+
+    const user = true
+
+    return (
+        <div className='w-full bg-white'>
+            <div className='header-top bg-[#caddff] md-lg:hidden'>
+                <div className='w-[85%] lg:w-[90%] mx-auto'>
+                    <div className='flex w-full justify-between items-center h-[50px] text-slate-500'>
+                        <ul className='flex justify-start items-center gap-8 font-semibold text-black'>
+                            <li className='flex relative justify-center items-center gap-2 text-sm after:absolute after:h-[18px] after:w-[1px] after:bg-[#afafaf] after:-right-[16px]'>
+                                <span><MdEmail /></span>
+                                <span>support@gmail.com</span>
+                            </li>
+
+                            <li className='flex relative justify-center items-center gap-2 text-sm '>
+                                <span><IoMdPhonePortrait  /></span>
+                                <span>+(90) 555 555 5555 </span>
+                            </li>
+
+                        </ul>
+
+                        <div>
+                            <div className='flex justify-center items-center gap-10'>
+                                 <div className='flex justify-center items-center gap-4 text-black'>
+                                     <a href="#"><FaFacebookF /></a>
+                                     <a href="#"><FaTwitter /> </a>
+                                     <a href="#"><FaLinkedin /></a>
+                                     <a href="#"><FaGithub /> </a> 
+                                 </div>
+                                    <div className='flex group cursor-pointer text-slate-800 text-sm justify-center items-center gap-1 relative after:h-[18px] after:w-[1px] after:bg-[#afafaf] after:-right-[16px] after:absolute before:absolute before:h-[18px] before:bg-[#afafaf] before:w-[1px] before:-left-[20px]'>
+                                        <img src="http://localhost:3000/images/language.png" alt="" />
+                                        <span><IoMdArrowDropdown /></span>
+                                        <ul className='absolute invisible transition-all top-12 rounded-sm duration-200 text-white p-2 w-[100px] flex flex-col gap-3 group-hover:visible group-hover:top-6 group-hover:bg-black z-10'>
+                                            <li>Türkçe</li>
+                                            <li>English</li>
+                                        </ul>
+                                    </div>
+
+                                    {
+                                        user ? <Link className='flex cursor-pointer justify-center items-center gap-2 text-sm text-black' to='/dashboard'>
+                                        <span> <FaUser/> </span>
+                                        <span>Mert Akmahmut</span>
+                                        </Link> : <Link className='flex cursor-pointer justify-center items-center gap-2 text-sm text-black' to='/login'>
+                                        <span> <FaLock /> </span>
+                                        <span>Login </span>
+                                        </Link>
+                                    }
+ 
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        
+        </div>
+    );
+};
+
+export default Header;
