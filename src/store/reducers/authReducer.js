2110cb3364adff072bfe99e4003faca367c4ab80
mertakmahmut <mertakmahmut@icloud.com>
2025-05-02 12:01:57 +0300

    customer register (loader)

---
+++ src/store/reducers/authReducer.js
@@ -0,0 +1,50 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import api from '../../api/api';
+
+export const customer_register = createAsyncThunk(
+    'auth/customer_register',
+    async(info, { rejectWithValue, fulfillWithValue}) => {
+        try {
+            const {data} = await api.post('/customer/customer-register', info)
+            localStorage.setItem('customerToken', data.token) 
+            // console.log(data)
+            return fulfillWithValue(data)
+        } catch (error) {
+            console.log(error.response)
+        }
+    }
+)
+
+export const authReducer = createSlice({
+    name : 'auth',
+    initialState : {
+        loader : false,
+        userInfo : '',
+        errorMessage : '',
+        successMessage : ''
+    },
+    reducers : {
+        messageClear : (state, _) => {
+            state.errorMessage = ""
+            state.successMessage = ""
+        }
+    },
+    extraReducers: (builder) => {
+        builder
+        .addCase(customer_register.pending, (state, { payload }) => {
+            state.loader = true;
+        })
+        .addCase(customer_register.rejected, (state, { payload }) => {
+            state.errorMessage = payload.error;
+            state.loader = false;
+        })
+        .addCase(customer_register.fulfilled, (state, { payload }) => {
+            state.successMessage = payload.message;
+            state.loader = false;
+        })
+
+    }
+})
+
+export const {messageClear} = authReducer.actions
+export default authReducer.reducer
